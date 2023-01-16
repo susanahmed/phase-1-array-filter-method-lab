9cde1aa42f85ee4f4ce3cbb1e1956b9c01b76762
susanahmed <susanahmed617@gmail.com>
2023-01-15 20:19:46 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+function findMatching(array, string) {
+    return array.filter(
+        almostExact => almostExact.toLowerCase() === string.toLowerCase()
+    )
+
+}
+
+function fuzzyMatch(array, string){
+    return array.filter(
+        (mehMatch) => mehMatch.toLowerCase().indexOf(string.toLowerCase()) === 0
+    )
+
+}
+
+function matchName(array, string){
+    const driverNames = array.filter((driver) => driver.name === string)
+    return driverNames;
+}
